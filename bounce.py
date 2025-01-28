196716ed3f25975b5db067aafa0595d1a2e5d599
Sylvio Mafort <sylviomafort@gmail.com>
2025-01-28 15:40:31 +0900

    Create bounce.py

---
+++ bounce.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import random
+
+class Ball:
+    def __init__(self, x, y, dx, dy, color):
+        self.x = x 
+        self.dx = dx
+        self.y = y 
+        self.dy = dy 
+        self.color = color 
+        self.mae = None 
+        
+    def color_change(self, canvas):  
+        new_color = "#{:02x}{:02x}{:02x}".format(
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255)
+        )
+        self.color = new_color
+        canvas.itemconfig(self.mae, fill=new_color)
+        
+    def move(self, canvas):
+        if self.mae is not None:
+            canvas.delete(self.mae)
+    
+        self.x = self.x + self.dx
+        self.y = self.y + self.dy
+        
+        self.mae = canvas.create_oval(self.x - 20, self.y - 20, self.x + 20, self.y + 20, fill = self.color, width = 0)
+        
+        if self.x >= canvas.winfo_width() - 20:
+            self.dx = -1
+            self.color_change(canvas)
+        
+        if self.x <= 20:
+            self.dx = +1
+            self.color_change(canvas)
+            
+        if self.y >= canvas.winfo_height() - 20:
+            self.dy = -1
+            self.color_change(canvas)
+            
+        if self.y <= 20:
+            self.dy = +1
+            self.color_change(canvas)
+    
+b = Ball(400, 10, 1, 1, "red")
+c = Ball(300, 131, 1, 1, "red")
+d = Ball(200, 282, 1, 1, "red")
+e = Ball(100, 303, 1, 1, "red")
+
+def loop():
+    b.move(canvas)
+    c.move(canvas)
+    d.move(canvas)
+    e.move(canvas)
+    root.after(10, loop)
+
+root=tk.Tk()
+root.geometry("600x400")
+    
+canvas = tk.Canvas(root, width = 600, height = 400, bg = "white")
+canvas.place(x=0, y=0)
+
+root.after(10, loop)
+
+root.mainloop()
+    
